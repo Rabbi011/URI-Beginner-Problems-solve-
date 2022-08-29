179629d7914713fe7e614eba1065ea0b76884794
RAKIBUL HASAN <rakibul15-13011@diu.edu.bd>
2022-08-29 21:10:21 +0600

    some problems done

--- 1957 Converting to Hexadecimal/Converting to Hexadecimal.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    long int a;
-    while(scanf("%ld",&a)!=EOF){
-        printf("%X\n",a);
-    }
-    return 0;
-}
--- 2234 Hot Dogs/Hot Dogs.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    double M, P;
-    scanf("%lf %lf", &M, &P);
-    printf("%.2lf\n", M/P);
-    return 0;
-}
